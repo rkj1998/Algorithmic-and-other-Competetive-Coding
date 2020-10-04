76f596cd371b694aa85c263ba20f88cd06b8adb6
rkj1998 <62897573+rkj1998@users.noreply.github.com>
2020-10-04 15:51:27 +0530

    Update and rename JavaApplication5.java to FindMotherNodeInAGraph.java

--- FindMotherNodeInAGraph.java
+++ FindMotherNodeInAGraph.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package javaapplication5;
 /*package whatever //do not write package name here */
 
 import java.util.*;
